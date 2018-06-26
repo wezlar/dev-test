f2910de6bdc958519d1691c7c88878b43f4a1341
Dean Pugh <dean@barkweb.co.uk>
2018-06-26 21:28:38 +0100

    Add in error catch for api and display better error message

--- public/index.php
+++ public/index.php
@@ -6,12 +6,18 @@ $loader = new Twig_Loader_Filesystem('../templates/');
 $twig = new Twig_Environment($loader, ['debug' => true]);
 
 //Get the episodes from the API
-$client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'http://3ev.org/dev-test-api/');
-$data = json_decode($res->getBody(), true);
+try {
+  $client = new GuzzleHttp\Client();
+  $res = $client->request('GET', 'http://3ev.org/dev-test-api/');
+  $data = json_decode($res->getBody(), true);
+  $error = '';
 
-//Sort the episodes
-array_multisort(array_keys($data), SORT_ASC, SORT_STRING, $data);
+  //Sort the episodes
+  array_multisort(array_keys($data), SORT_ASC, SORT_STRING, $data);
+} catch (\GuzzleHttp\Exception\RequestException $e) {
+  $data = [];
+  $error = 'Sorry, there was an error. Please try again by reloading the page.';
+}
 
 //Render the template
-echo $twig->render('page.html', ["episodes" => $data]);
+echo $twig->render('page.html', ["episodes" => $data, "error" => $error]);
